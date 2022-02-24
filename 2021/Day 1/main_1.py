7c54a40f36a3c482395fff39df7ef846a1772669
Milos Stamenkovic <mstamenkovic3@gmail.com>
2022-02-24 11:22:44 +0100

    Day 1

---
+++ 2021/Day 1/main_1.py
@@ -0,0 +1,8 @@
+x = [int(line.strip()) for line in open('input_1.txt')]
+counter = 0
+
+for i in range(1, len(x)):
+    if x[i] > x[i-1]:
+        counter += 1
+
+print(counter, "measurements are larger than the previous.")
